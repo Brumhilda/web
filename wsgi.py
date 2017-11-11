07bf3ad8bec54c480d217f84053924be7dd76390
ElizavetaAlekseevna <lizka030697@mail.ru>
2017-11-11 20:41:48 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
-from wsgiref import simple_server, validate
+from webob import Request
+from jinja2 import Environment, FileSystemLoader
 
 includes = [
                 'app.js',
@@ -11,46 +12,34 @@ includes = [
                 'bootstrap.css',
                 'normalize.css',
             ]
-	    
+environment = Environment(loader = FileSystemLoader('.'))	    
 class WsgiTopBottomMiddleware(object):
     def __init__(self, app):
         self.app = app
 
     def __call__(self, environ, start_response):       
     
-            css = ""
-            js = ""
+            css = []
+            js = []
             for i in includes:
                 if(i.split('.')[1] == 'css'):
-                    css += '<link rel="stylesheet" href="/_static/' + i + '"/>\n'
+                    css.append(i)
                 else:
-                    js += '<script src="/_static/' + i + '"></script>\n'
+                    js.append(i)
 	    
             response = self.app(environ, start_response).decode() 
-            if response.find('<head>') > -1:
-                data, headend = response.split('</head>')
-                response = data + css + '</head>' + headend
-            if response.find('<body>') > -1:
-                data, htmlend = response.split('</body>')
-                response = data + js +'</body>' + htmlend
-            return (response).encode()  
-	    
+            return (response).encode()  	    
 
 def app(environ, start_response):
     addr = environ['PATH_INFO']
     path = '.' + addr  
-    file = open(path, 'r')
-    fileread = file.read()
-    file.close()
-
     response_code = '200 OK'
     response_type = ('Content-Type', 'text/HTML')
     start_response(response_code, [response_type])
-    return fileread
+    template = enviroment.get_template(path)
+    return template
 
 # Оборачиваем WSGI приложение в middleware
 app = WsgiTopBottomMiddleware(app)
 
-
-# Запускаем сервер
-from wsgirev serve(app, host='localhost', port=8000)
+print(Request.blank('/index.html').get_response(app))
